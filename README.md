ec43a95644f6de5f604349b0a9b77d2441e9e00f
georgemac510 <georgemac510@gmail.com>
2022-05-22 09:03:28 -0400

    Add frontend

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # zkSync Portfolio Token
 
 zkSync Portfolio Token is a diversified crypto portfolio investment DeFi token that includes well-researched, low-risk yet higher-yield alternatives to current platforms. Deploying to zkSync allows for inexpensive gas transactions as well as transactions deployed to the Ethereum EVM via zk rollups.  
+
+# Steps to deploy contract and run app
+
+    
---
+++ dapp.js
@@ -0,0 +1,445 @@
+// contract address on zkSync testnet:
+const ssAddress = '0x4cf0EB256f67eb0059BE03e9DD2B4a534376F9da'
+
+// add contract ABI from Remix:
+
+const ssABI =
+[
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "subtractedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "addedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "mint",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
+// Using the 'load' event listener for Javascript to
+// check if window.ethereum is available
+
+window.addEventListener('load', function() {
+  
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('window.ethereum is enabled')
+    if (window.ethereum.isMetaMask === true) {
+      console.log('MetaMask is active')
+      let mmDetected = document.getElementById('mm-detected')
+      mmDetected.innerHTML += 'MetaMask Is Available!'
+
+      // add in web3 here
+      var web3 = new Web3(window.ethereum)
+
+    } else {
+      console.log('MetaMask is not available')
+      let mmDetected = document.getElementById('mm-detected')
+      mmDetected.innerHTML += 'MetaMask Not Available!'
+      // let node = document.createTextNode('<p>MetaMask Not Available!<p>')
+      // mmDetected.appendChild(node)
+    }
+  } else {
+    console.log('window.ethereum is not found')
+    let mmDetected = document.getElementById('mm-detected')
+    mmDetected.innerHTML += '<p>MetaMask Not Available!<p>'
+  }
+})
+
+
+var web3 = new Web3(window.ethereum)
+
+// Grabbing the button object,  
+
+const mmEnable = document.getElementById('mm-connect');
+
+// since MetaMask has been detected, we know
+// `ethereum` is an object, so we'll do the canonical
+// MM request to connect the account. 
+// 
+// typically we only request access to MetaMask when we
+// need the user to do something, but this is just for
+// an example
+ 
+mmEnable.onclick = async () => {
+  await ethereum.request({ method: 'eth_requestAccounts'})
+  // grab mm-current-account
+  // and populate it with the current address
+  var mmCurrentAccount = document.getElementById('mm-current-account');
+  mmCurrentAccount.innerHTML = 'Current Account: ' + ethereum.selectedAddress
+}
+
+// grab the button for input to a contract:
+
+const ssSubmit = document.getElementById('ss-input-button');
+
+ssSubmit.onclick = async () => {
+  // grab value from input
+  
+  const ssInputValue = document.getElementById('ss-input-box').value;
+  console.log(ssInputValue)
+
+  var web3 = new Web3(window.ethereum)
+
+  // instantiate smart contract instance
+  
+  const simpleStorage = new web3.eth.Contract(ssABI, ssAddress)
+  simpleStorage.setProvider(window.ethereum)
+
+  await simpleStorage.methods.store(ssInputValue).send({from: ethereum.selectedAddress})
+
+}
+
+const ssGetValue = document.getElementById('ss-get-value')
+
+ssGetValue.onclick = async () => {
+
+  var web3 = new Web3(window.ethereum)
+
+  const simpleStorage = new web3.eth.Contract(ssABI, ssAddress)
+  simpleStorage.setProvider(window.ethereum)
+
+  var value = await simpleStorage.methods.retrieve().call()
+
+  console.log(value)
+
+  const ssDisplayValue = document.getElementById('ss-display-value')
+
+  ssDisplayValue.innerHTML = 'Current Simple Storage Value: ' + value
+
+}
